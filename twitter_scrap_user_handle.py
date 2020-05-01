034210a90d0b339f5aa534f1018aebed00283289
achhetr <akash.chhetri1@gmail.com>
2020-05-01 19:14:20 +1000

    04- removed urls, new coloumn for url and text filtered

--- twitter_scrap_user_handle.py
+++ twitter_scrap_user_handle.py
@@ -1,11 +1,10 @@
 
 # import libraries
 from twitterscraper import query_tweets_from_user
-import datetime as dt
 import pandas as pd
 
 # number of tweets
-limit = None
+limit = 5
 
 # user tweets
 user = input('Enter twitter username without @: ')
@@ -21,6 +20,28 @@ df = pd.DataFrame(tweet.__dict__ for tweet in tweets)
 # clean tweets and remove retweets
 df['screen_name'] = df['screen_name'].apply(lambda x: x.lower())
 df = df[df['screen_name'] == user]
+
+# convert user text to UTF-8 ---------------------Work in progress
+df['text'] = df['text'].apply(lambda x: x)
+
+# convert user text to lowercase
+df['text'] = df['text'].apply(lambda x: x.lower())
+
+# remove all urls
+def remove_url_from_text(text):
+    t = get_url_from_text(text)
+    return text.replace(t,'')
+
+def get_url_from_text(text):
+    text_arr = text.split(' ')
+    for t in text_arr:
+        if("https://") in t: 
+            return t[t.find('https://'):]
+    return 'NA'
+
+df['message_text_only'] = df['text'].apply(lambda x: remove_url_from_text(x))
+df['url_links'] = df['text'].apply(lambda x: get_url_from_text(x))
+
 
 # output in csv
 df.to_csv(user + '-output.csv',index=False)
